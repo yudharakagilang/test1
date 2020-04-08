4d3906f65ee068345b45bac57167618e5e3868c0
yudharakagilang <47841651+yudharakagilang@users.noreply.github.com>
2020-04-08 20:20:54 +0700

    test

--- Untitled-3.sql
+++ Untitled-3.sql
@@ -3,7 +3,7 @@ count(Status) filter(where Status = 'Normal') as jml_status_normal,
 count(Status) filter(where Status = 'Rawan') as jml_status_rawan,
 count(Status) filter(where Status = 'Waspada') as jml_status_waspada,
 count(Status) filter(where Status = 'Kritis') as jml_status_kritis
-from tinggi_permukaan_air_pintu_air group by nama_pintu_air;
+
 
 
 
